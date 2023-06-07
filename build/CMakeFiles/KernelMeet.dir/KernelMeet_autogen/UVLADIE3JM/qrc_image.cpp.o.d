b3072d0f6212754bfad57f8e2353449ed9d40fc9
ranux <2696493143@qq.com>
2023-06-07 19:43:08 +0800

    delete build dir

--- build/CMakeFiles/KernelMeet.dir/KernelMeet_autogen/UVLADIE3JM/qrc_image.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/KernelMeet.dir/KernelMeet_autogen/UVLADIE3JM/qrc_image.cpp.o: \
- /home/ran/qtcode/KernelMeeting/build/KernelMeet_autogen/UVLADIE3JM/qrc_image.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/KernelMeet.dir/KernelMeet_autogen/UVLADIE3JM/qrc_other.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/KernelMeet.dir/KernelMeet_autogen/UVLADIE3JM/qrc_other.cpp.o: \
- /home/ran/qtcode/KernelMeeting/build/KernelMeet_autogen/UVLADIE3JM/qrc_other.cpp \
- /usr/include/stdc-predef.h
